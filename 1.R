66d69135bd357973aa2b29d08e27f5c9a537b818
HelenSanma <littlemiss330@gmail.com>
2016-05-22 21:58:52 +0800

    跑跑看回歸

--- 1.R
+++ 1.R
@@ -4,6 +4,7 @@ readData <- function(filename ){
 
 	data <- read.table(filename, header = TRUE, sep = ",", stringsAsFactors = FALSE,skip=1)
 	data <- data[-1,]
+	data <-  as.data.frame(apply(data,2,as.numeric))
 	return(data)
 }
 checkTheSameColumn　<-function(data){
@@ -37,7 +38,7 @@ season <- readData("season.csv")
 year_y_names<-c("成交股數...股票..千股.","成交金額...股票..新台幣千元.","成交筆數...總筆數..千股.",
 	"上市公司家數..新台幣千元.","上市總股數..千股.","上市股份面值總額..新台幣千元.",
 	"上市股份市值總額.期終日...新台幣千元.","增資總金額..新台幣千元.",
-	"發行量加權股價指數..基期..民國.55.年.100.","平均每日成交股數...股票..千股.",
+	"發行量加權股價指數..基期..民國.55.年.100.","股價指數..基期..民國.55.年.100.","平均每日成交股數...股票..千股.",
 	"平均每日成交金額...股票..千股.","上市股票...公司家數..家.","上市股票...總面值..新台幣百萬元.",
 	"上市股票...總市值..新台幣百萬元.","上市股票...總成交值..新台幣百萬元.",
 	"上市股票...日平均成交值..新台幣百萬元.")
@@ -58,3 +59,14 @@ month_x <- month_s$x
 # season_y <- season_s$y
 # season_x <- season_s$x
 
+plot(cbind(year_y[,1],year_x[,c(1:6)]))
+plot(cbind(year_y[,1],year_x[,c(7:12)]))
+plot(cbind(year_y[,1],year_x[,c(13:18)]))
+plot(cbind(year_y[,1],year_x[,c(19:24)]))
+
+##好像相關的
+year_relevant_names <- c("利率...貨幣市場利率..年率..","匯率..新台幣.外幣....美金","經濟成長率....",
+	"國民所得毛額..按當期價格計算...百萬美元.","國內生產毛額...中華民國..美金十億元.","經濟成長率...美國....")
+year_relevant <- year[year_relevant_names]
+year_anal <- cbind(y=year_y[,1],year_relevant)
+m <- lm(y~. , data=year_anal)
